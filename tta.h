a2752fdb2495e9d4fc4e32423d8ddcf999df2a6d
Charlie Friend <charles.d.friend@gmail.com>
2019-03-07 14:42:35 -0800

    Add header guards to tta.h

--- tta.h
+++ tta.h
@@ -1,9 +1,16 @@
+#include <stdint.h>
+
+#ifndef TTA_H
+#define TTA_H
+
 #define MAXTASKS	8
 
 typedef void (*voidfuncptr) (void);      /* pointer to void f(void) */ 
 
 void Scheduler_Init();
 
-void Scheduler_StartTask(int16_t delay, int16_t period, task_cb task);
+void Scheduler_StartTask(int16_t delay, int16_t period, voidfuncptr task);
 
 uint32_t Scheduler_Dispatch();
+
+#endif
