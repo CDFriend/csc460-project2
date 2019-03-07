a3493a618c5c28fb0098754df8d70c8900be11e1
AndrewBraun <ABandrewbraun@gmail.com>
2019-03-07 14:50:39 -0800

    Update tta.h

--- tta.h
+++ tta.h
@@ -1,9 +1,11 @@
-#include <stdint.h>
-
 #ifndef TTA_H
 #define TTA_H
 
+// The maximum number of tasks, not including the e
 #define MAXTASKS	8
+
+#define Disable_Interrupt()		asm volatile ("cli"::)
+#define Enable_Interrupt()		asm volatile ("sei"::)
 
 typedef void (*voidfuncptr) (void);      /* pointer to void f(void) */ 
 
@@ -11,6 +13,6 @@ void Scheduler_Init();
 
 void Scheduler_StartTask(int16_t delay, int16_t period, voidfuncptr task);
 
-uint32_t Scheduler_Dispatch();
+uint16_t Scheduler_Dispatch();
 
 #endif
