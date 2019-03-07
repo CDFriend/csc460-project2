ac7bc13676c1679cb37a512850f3b41cb91cf907
AndrewBraun <ABandrewbraun@gmail.com>
2019-03-07 14:35:59 -0800

    Update tta.h

--- tta.h
+++ tta.h
@@ -1,7 +1,9 @@
-/**
- * tta.h
- * ------
- * Time-triggered scheduler implementation.
- */
+#define MAXTASKS	8
 
+typedef void (*voidfuncptr) (void);      /* pointer to void f(void) */ 
 
+void Scheduler_Init();
+
+void Scheduler_StartTask(int16_t delay, int16_t period, task_cb task);
+
+uint32_t Scheduler_Dispatch();
