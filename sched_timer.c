87b3e23a0e21bdc00b8df34bcb44fa5e1c9e46c1
Charlie Friend <charles.d.friend@gmail.com>
2019-03-09 14:28:15 -0800

    Fix timer scheduler

--- sched_timer.c
+++ sched_timer.c
@@ -10,14 +10,15 @@
 #define Disable_Interrupt()		asm volatile ("cli"::)
 #define Enable_Interrupt()		asm volatile ("sei"::)
 
-static volatile int16_t g_timerTicks;
+static volatile int16_t g_timerMs = 0;  // milliseconds
+static volatile int16_t g_timerMsFractional = 0;  // thousandths of a millisecond
 
 void Schedtimer_Init() 
 {
-    TCCR1B |= (1 << CS10);  // No prescaling
-    TCCR1B |= (1 << WGM12);  // Set to CTC mode
-    OCR1A = 16000;  // 1 msec = 16000 ticks
-    TIMSK1 |= (1 << OCIE1A);  // Enable timer 0 compare interrupt
+    TCCR1B |= ((1<<CS12)|(1<<CS10));  // Prescale: (clock / 1024)
+
+    OCR1A = 16;  // 1.024 msec = 16 ticks
+    TIMSK1 |= (1 << OCIE1A);  // Enable timer 1 compare interrupt
 
     // Init timer debug pin (PA0/Digital pin 22)
     DDRA |= (1 << PA0);
@@ -26,14 +27,29 @@ void Schedtimer_Init()
 
 int16_t millis()
 {
-    return g_timerTicks;
+    return g_timerMs;
 }
 
 SIGNAL(TIMER1_COMPA_vect)
 {
     Disable_Interrupt();
     PORTA |= (1 << PA0);
-    g_timerTicks++;
+
+    // Our prescaler uses (clock / 1024), which at 16Mhz gives 15625 cycles/sec.
+    // At this rate, 16 ticks gives us 1.024 ms, so we'll track both the whole
+    // part (1 ms every 16 ticks) and the fractional part (0.024ms every 16 ticks)
+    // to get an accurate time.
+    uint16_t ticks = TCNT1 / 16;
+    g_timerMs += ticks;  // whole part (1ms)
+
+    // Calculate fractional part
+    g_timerMsFractional = g_timerMsFractional + 24 * ticks;
+    g_timerMs += g_timerMsFractional / 1000;
+    g_timerMsFractional = g_timerMsFractional % 1000;
+
+    // Reset TCNT1
+    TCNT1 = TCNT1 % 16;
+
     PORTA &= ~(1 << PA0);
     Enable_Interrupt();
 }
