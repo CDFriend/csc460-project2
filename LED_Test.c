f23ff6ede0821aa2fa6e1be73f5acc86b9385a7c
AndrewBraun <ABandrewbraun@gmail.com>
2019-03-09 14:06:53 -0800

    Update LED_Test.c

--- LED_Test.c
+++ LED_Test.c
@@ -11,30 +11,22 @@
  * \date October 2006
  */
 
-void init_LED_B2(void)
-{
-	//DDRD |= LED_D2;		//Set LED to output (pins 4 and 5)
-	//PORTD = 0x00;		//Initialize port to LOW (turn off LEDs)
-	DDRB |= LED_B2;
+void init_LED_B2(void){
+	//DDRB |= LED_B2;
+	DDRB = 0xFF;
 	PORTB = 0x00;
 }
 
-void init_LED_B5(void)
-{
-	//DDRD |= LED_D5;		//Set LED to output (pins 6 and 7)
-	//PORTD = 0x00;		//Initialize port to LOW (turn off LEDs)
-	DDRB |= LED_B5;
+void init_LED_B5(void){
+	//DDRB |= LED_B5;
+	DDRB = 0xFF;
 	PORTB = 0x00;
 }
 
-void enable_LED(unsigned int mask)
-{
-	//PORTD = mask;		//Initialize port to high
+void enable_LED(unsigned int mask){
 	PORTB = mask;
 }
 
-void disable_LEDs(void)
-{
-		//PORTD = 0x00;	//Initialize port to high
-		PORTB = 0x00;
+void disable_LEDs(void){
+	PORTB = 0x00;
 }
